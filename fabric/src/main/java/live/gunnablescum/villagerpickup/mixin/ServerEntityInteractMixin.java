4b0da0b4a2168464342fa571a9f5ec0dd680fd6f
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2025-10-19 22:55:39 +0200

    Remove unused imports

--- fabric/src/main/java/live/gunnablescum/villagerpickup/mixin/ServerEntityInteractMixin.java
+++ fabric/src/main/java/live/gunnablescum/villagerpickup/mixin/ServerEntityInteractMixin.java
@@ -1,9 +1,7 @@
 package live.gunnablescum.villagerpickup.mixin;
 
 import live.gunnablescum.villagerpickup.CommonClass;
-import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
@@ -11,7 +9,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
