893e611866baf180cabacac37d1aee55e0dad992
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2026-08-30 09:12:08 +0200

    Remove Javadocs & Minimize non-common Code

--- neoforge/src/main/java/live/gunnablescum/villagerpickup/platform/NeoForgePlatformHelper.java
+++ neoforge/src/main/java/live/gunnablescum/villagerpickup/platform/NeoForgePlatformHelper.java
@@ -3,6 +3,7 @@ package live.gunnablescum.villagerpickup.platform;
 import live.gunnablescum.villagerpickup.platform.services.IPlatformHelper;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.fml.loading.FMLPaths;
 
 import java.io.File;
 
@@ -10,24 +11,21 @@ public class NeoForgePlatformHelper implements IPlatformHelper {
 
     @Override
     public String getPlatformName() {
-
         return "NeoForge";
     }
 
     @Override
     public boolean isModLoaded(String modId) {
-
         return ModList.get().isLoaded(modId);
     }
 
     @Override
     public boolean isDevelopmentEnvironment() {
-
         return !FMLLoader.getCurrent().isProduction();
     }
 
     @Override
     public File getConfigDirectory() {
-        return net.neoforged.fml.loading.FMLPaths.CONFIGDIR.get().toFile();
+        return FMLPaths.CONFIGDIR.get().toFile();
     }
 }
