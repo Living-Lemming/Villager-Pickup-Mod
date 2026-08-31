37c6034059b07ca66f264599b5b04272c64dfd84
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2026-08-31 12:06:48 +0200

    Remove unused PlatformHelper functions

--- fabric/src/main/java/live/gunnablescum/villagerpickup/platform/FabricPlatformHelper.java
+++ fabric/src/main/java/live/gunnablescum/villagerpickup/platform/FabricPlatformHelper.java
@@ -1,31 +1,15 @@
 package live.gunnablescum.villagerpickup.platform;
 
-import live.gunnablescum.villagerpickup.Constants;
 import live.gunnablescum.villagerpickup.platform.services.IPlatformHelper;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.File;
 
+@SuppressWarnings("unused") // It's used, loaded dynamically.
 public class FabricPlatformHelper implements IPlatformHelper {
 
     @Override
-    public String getPlatformName() {
-        return "Fabric";
-    }
-
-    @Override
-    public boolean isModLoaded(String modId) {
-        return FabricLoader.getInstance().isModLoaded(modId);
-    }
-
-    @Override
-    public boolean isDevelopmentEnvironment() {
-        return FabricLoader.getInstance().isDevelopmentEnvironment();
-    }
-
-    @Override
     public File getConfigDirectory() {
-        Constants.LOG.info("Accessing config directory via Fabric API");
         return FabricLoader.getInstance().getConfigDir().toFile();
     }
 }
--- neoforge/src/main/java/live/gunnablescum/villagerpickup/platform/NeoForgePlatformHelper.java
+++ neoforge/src/main/java/live/gunnablescum/villagerpickup/platform/NeoForgePlatformHelper.java
@@ -1,28 +1,12 @@
 package live.gunnablescum.villagerpickup.platform;
 
 import live.gunnablescum.villagerpickup.platform.services.IPlatformHelper;
-import net.neoforged.fml.ModList;
-import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
 
 import java.io.File;
 
+@SuppressWarnings("unused") // It's used, loaded dynamically.
 public class NeoForgePlatformHelper implements IPlatformHelper {
-
-    @Override
-    public String getPlatformName() {
-        return "NeoForge";
-    }
-
-    @Override
-    public boolean isModLoaded(String modId) {
-        return ModList.get().isLoaded(modId);
-    }
-
-    @Override
-    public boolean isDevelopmentEnvironment() {
-        return !FMLLoader.getCurrent().isProduction();
-    }
 
     @Override
     public File getConfigDirectory() {
