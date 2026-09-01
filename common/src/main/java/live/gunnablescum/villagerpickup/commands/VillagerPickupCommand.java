2f59618e875f8e9d3b8b2a67c98a83f7788d32f3
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2026-09-01 09:26:47 +0200

    Mmmmmmmmm readability

--- common/src/main/java/live/gunnablescum/villagerpickup/commands/VillagerPickupCommand.java
+++ common/src/main/java/live/gunnablescum/villagerpickup/commands/VillagerPickupCommand.java
@@ -1,6 +1,7 @@
 package live.gunnablescum.villagerpickup.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
 import live.gunnablescum.villagerpickup.configuration.ConfigurationElement;
 import live.gunnablescum.villagerpickup.configuration.ConfigurationHandler;
 import live.gunnablescum.villagerpickup.configuration.ConfigurationScreenHandler;
@@ -18,33 +19,43 @@ public class VillagerPickupCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(literal("villager-pickup")
-                .then(literal("status").executes(context -> {
-                    CommandSourceStack css = context.getSource();
-                    css.sendSystemMessage(Component.literal("Villager-Pickup Status:").withStyle(ChatFormatting.GOLD));
-                    css.sendSystemMessage(getDescribedCfgOption(ConfigurationElement.ENABLE_VILLAGER_PICKUP));
-                    css.sendSystemMessage(getDescribedCfgOption(ConfigurationElement.ALLOW_VILLAGER_ANVIL_RENAME));
-                    return 1;
-                }))
-                .then(literal("reload").requires(source -> source.permissions().hasPermission(Permissions.COMMANDS_OWNER)).executes(context -> {
-                    if(ConfigurationHandler.reloadConfig()) {
-                        context.getSource().sendSuccess(() -> Component.literal("Config Reload successful.").withStyle(ChatFormatting.GREEN), true);
-                        return 1;
-                    }
-                    context.getSource().sendFailure(Component.literal("Failure to load Configuration. Check Console."));
-                    return 1;
-                }))
-                .then(literal("config-gui").requires(source -> source.permissions().hasPermission(Permissions.COMMANDS_OWNER)).executes(context -> {
-                    if(context.getSource().isPlayer()) {
-                        ServerPlayer player = context.getSource().getPlayer();
-                        context.getSource().sendSuccess(() -> Component.literal("Editing Villager-Pickup Config...").withStyle(ChatFormatting.GRAY), true);
-                        player.openMenu(new SimpleMenuProvider((syncId, playerInventory, _) -> new ConfigurationScreenHandler(syncId, playerInventory), Component.literal("Villager-Pickup Config")));
-                    } else {
-                        context.getSource().sendFailure(Component.literal("This command can only be executed by a player."));
-                        return 0;
-                    }
-                    return 1;
-                }))
+                .then(literal("status").executes(VillagerPickupCommand::configurationStatus))
+                .then(literal("reload").requires(VillagerPickupCommand::isOperator).executes(VillagerPickupCommand::reloadConfiguration))
+                .then(literal("config-gui").requires(VillagerPickupCommand::isOperator).executes(VillagerPickupCommand::openConfigurationMenu))
         );
+    }
+
+    private static boolean isOperator(CommandSourceStack css) {
+        return css.permissions().hasPermission(Permissions.COMMANDS_OWNER);
+    }
+
+    private static int configurationStatus(CommandContext<CommandSourceStack> context) {
+        CommandSourceStack css = context.getSource();
+        css.sendSystemMessage(Component.literal("Villager-Pickup Status:").withStyle(ChatFormatting.GOLD));
+        css.sendSystemMessage(getDescribedCfgOption(ConfigurationElement.ENABLE_VILLAGER_PICKUP));
+        css.sendSystemMessage(getDescribedCfgOption(ConfigurationElement.ALLOW_VILLAGER_ANVIL_RENAME));
+        return 1;
+    }
+
+    private static int reloadConfiguration(CommandContext<CommandSourceStack> context) {
+        if(ConfigurationHandler.reloadConfig()) {
+            context.getSource().sendSuccess(() -> Component.literal("Config Reload successful.").withStyle(ChatFormatting.GREEN), true);
+            return 1;
+        }
+        context.getSource().sendFailure(Component.literal("Failure to load Configuration. Check Console."));
+        return 0;
+    }
+
+    private static int openConfigurationMenu(CommandContext<CommandSourceStack> context) {
+        if(context.getSource().isPlayer()) {
+            ServerPlayer player = context.getSource().getPlayer();
+            context.getSource().sendSuccess(() -> Component.literal("Editing Villager-Pickup Config..."), true);
+            //noinspection DataFlowIssue (player is not null, because there is a isPlayer check above)
+            player.openMenu(new SimpleMenuProvider((syncId, playerInventory, _) -> new ConfigurationScreenHandler(syncId, playerInventory), Component.literal("Villager-Pickup Config")));
+            return 1;
+        }
+        context.getSource().sendFailure(Component.literal("This command can only be executed by a player."));
+        return 0;
     }
 
 }
